2226d9a0909e4b3396e46173c8ae5b4e91c76ebc
Arthur Franchini <arthurfranchini@gurgelmix.com.br>
2022-04-01 13:38:20 -0300

    added linking

--- App.js
+++ App.js
@@ -1,41 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as Linking from 'expo-linking';
 import LoginPage from './src/pages/LoginPage';
 import GamesListPage from './src/pages/GamesListPage';
 import { UserProvider } from './src/contexts/user/UserContext';
 
+const prefix = Linking.createURL('/');
+
 const Stack = createStackNavigator();
 
-const App = () => (
-  <NavigationContainer>
-    <UserProvider>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerMode: 'float' }}
-      >
-        <Stack.Screen
-          options={{
-            headerTitleAlign: 'center',
-            title: 'Login',
-            headerShown: false,
-          }}
-          name="LoginPage"
-          component={LoginPage}
-        />
-        <Stack.Screen
-          options={{
-            headerTitleAlign: 'center',
-            title: 'Games',
-            // headerLeft: () => null,
-          }}
-          name="GamesListPage"
-          component={GamesListPage}
-        />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </UserProvider>
-  </NavigationContainer>
-);
+const App = () => {
+  const linking = {
+    prefixes: [prefix],
+  };
+
+  return (
+    <NavigationContainer linking={linking}>
+      <UserProvider>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerMode: 'float' }}
+        >
+          <Stack.Screen
+            options={{
+              headerTitleAlign: 'center',
+              title: 'Login',
+              headerShown: false,
+            }}
+            name="LoginPage"
+            component={LoginPage}
+          />
+          <Stack.Screen
+            options={{
+              headerTitleAlign: 'center',
+              title: 'Games',
+              // headerLeft: () => null,
+            }}
+            name="GamesListPage"
+            component={GamesListPage}
+          />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </UserProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
